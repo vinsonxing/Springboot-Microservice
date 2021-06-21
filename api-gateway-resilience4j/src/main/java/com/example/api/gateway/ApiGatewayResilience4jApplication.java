81ba48cb2426de0af55cb08d5e5d0196258b23c1
Vinson Xing <vixing@cisco.com>
2021-06-21 18:38:19 +0800

    add resilience

---
+++ api-gateway-resilience4j/src/main/java/com/example/api/gateway/ApiGatewayResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayResilience4jApplication.class, args);
+	}
+
+}
